861dc16a48d9382fc74e972a5eb658bd769777eb
Vinicius <viniciusvill300@gmail.com>
2023-10-31 09:30:32 -0300

    chore: update test

--- calculadora.test.js
+++ calculadora.test.js
@@ -6,6 +6,7 @@ describe('Teste da Calculadora', () =>{
       expect(somar(-1,-1)).toBe(-2);
       expect(somar(2,2)).toBe(4);
       expect(somar(10,10)).toBe(20);
+      expect(somar(10,1)).toBe(11);
     });
     
 });
